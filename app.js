22c9b5f0e046011a4f07d2ad638961a4e085c120
Zomer <zomerzxc.19@gmail.com>
2021-08-21 08:40:28 +0800

    Add custom middleware

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
+    date: req.requestTime,
     results: tours.length,
     data: {
       tours,
