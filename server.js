93a1844d5f0ba6856b6635811205ee4957d36f6b
Zomer <zomerzxc.19@gmail.com>
2021-08-29 18:34:52 +0800

    Add handler for unhandled rejection promises

--- server.js
+++ server.js
@@ -21,8 +21,14 @@ mongoose
   .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-// TEST
+process.on('unhandledRejection', (err) => {
+  console.log('ERROR 😡', err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
