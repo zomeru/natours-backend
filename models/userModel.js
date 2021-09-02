686f1ad5a7555558baa5fa332c20d2fdc141e527
Zomer <zomerzxc.19@gmail.com>
2021-09-02 20:26:03 +0800

    Add password hashing - bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,7 +25,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on SAVE!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords do not match',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password is modified
+  if (!this.isModified('password')) return next();
+
+  // hash the password
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // delete password confirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
