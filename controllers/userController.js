85427da0f81358c225dbb06ff3eb3eb51a4c65e7
Zomer <zomerzxc.19@gmail.com>
2021-09-06 02:32:39 +0800

    Add update user data functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObject = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObject[el] = obj[el];
+  });
+
+  return newObject;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const user = await User.find();
@@ -10,6 +20,34 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: user.length,
     data: {
       user,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for updating password. Please use updateMyPassword',
+        400
+      )
+    );
+  }
+
+  // 2) Filtered out field names which are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
