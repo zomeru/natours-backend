75907b04cdae22a61b3045b79d8d9cbf149eadc2
Zomer <zomerzxc.19@gmail.com>
2021-09-06 18:07:49 +0800

    Implement http request limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,10 +9,18 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//? 1. MIDDLEWARES
+//? 1. GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in an hour.',
+});
+
+app.use('/api/', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -27,15 +36,7 @@ app.use((req, res, next) => {
   next();
 });
 
-//? 2. ROUTE HANDLERS
-
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-//? 3. ROUTES
+//? 2. ROUTES
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
