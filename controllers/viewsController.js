58cc36b4dabb414211bfd5d781f3583474d29bbc
Zomer <zomerzxc.19@gmail.com>
2021-09-12 22:13:14 +0800

    Build the tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,8 +15,16 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  //? 1. Get the data for the request tour (inluding reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  //? 2. Render Template using data from 1
   res.status(200).render('tour', {
     title: 'The Forest Hiker',
+    tour,
   });
-};
+});
